fc9bce7ec0619fed991d090fe2754f8ff3a02699
Lugel <lugel1996@gmail.com>
2024-01-11 13:38:23 -0500

    first commit

---
+++ src/main/java/com/makaia/gha/pruebagha/PruebaGhaApplication.java
@@ -0,0 +1,13 @@
+package com.makaia.gha.pruebagha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaGhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaGhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makaia/gha/pruebagha/PruebaGhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makaia.gha.pruebagha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaGhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
