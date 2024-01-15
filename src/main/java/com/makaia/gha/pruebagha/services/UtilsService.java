b21527dfa08e54137310cd11c18e81c0fdd8594a
Lugel <lugel1996@gmail.com>
2024-01-15 13:28:43 -0500

    Add UtilService

---
+++ src/main/java/com/makaia/gha/pruebagha/services/UtilsService.java
@@ -0,0 +1,9 @@
+package com.makaia.gha.pruebagha.services;
+
+public class UtilsService {
+
+    public static String transformTextToUppercase(String text){
+        return text.toUpperCase();
+    }
+
+}
